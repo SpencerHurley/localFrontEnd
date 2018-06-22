cfc2d0d77130c0e3a962ce541be1642c81ff3bb5
Spencer Hurley <spencer@Spencers-MacBook-Air.local>
2018-06-22 15:51:54 -0400

    Routing working

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,14 +4,15 @@ import {LoginComponent} from "./login/login.component";
 import {RegisterComponent} from "./register/register.component";
 import {ProfileComponent} from "./profile/profile.component";
 import {AdminPageComponent} from "./admin-page/admin-page.component";
+import {HomeComponent} from "./home/home.component";
 
 const appRoutes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full'},
-  { path: 'home', component: WhiteBoardComponent},
+  { path: 'home', component: HomeComponent},
   { path: 'login', component: LoginComponent},
   { path: 'register', component: RegisterComponent},
   { path: 'profile', component: ProfileComponent},
   { path: 'admin', component: AdminPageComponent },
-  { path: '**', component: WhiteBoardComponent} // last
+  { path: '**', component: HomeComponent} // last
 ];
 export const routing = RouterModule.forRoot(appRoutes);
